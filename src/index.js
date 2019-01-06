f07708a6314b119eed6a6f45b6002b85f441ef08
Vineet <vineet.kumar@dikonia.in>
2019-01-06 13:18:39 +0530

    Working demo

--- src/index.js
+++ src/index.js
@@ -1,5 +1,2 @@
 //import FormContainer from "./js/components/container/FormContainer.jsx";
 import index from "./js/index";
-store.subscribe(() => console.log("Look Me, Redux!"))
-store.dispatch( addArticle({ title: "React Redux Tutorial", id: 1}) )
-console.log(store.getState());
--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,12 @@
-import store from "../js/store/index";
-import { addArticle } from "../js/actions/index";
+import React from "react";
+import { render } from "react-dom";
+import { Provider } from "react-redux";
+import store from "./store/index";
+import App from "./components/App.jsx";
 
-window.store = store;
-window.addArticle = addArticle;
+render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById("root")
+);

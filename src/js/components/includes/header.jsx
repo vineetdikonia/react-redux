c96e142b53ad62f551972a2f355cec1d686efd3c
Vineet <vineet.kumar@dikonia.in>
2019-01-07 10:39:03 +0530

    Addde menu

---
+++ src/js/components/includes/header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => (
+    <div className="drop-down">
+        <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+    </div>
+);
+
+export default Header;
